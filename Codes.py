3a92f0a213691cc1b12b641231683965c2abc868
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2021-09-02 21:46:10 +0530

    Create Codes.py

---
+++ Codes.py
@@ -0,0 +1,5 @@
+Hey...What are you doing here 😂😂😂
+you opened this means you are Kanging this bot😑
+
+•We Don't like....But still Check Below For Codes•
+•DON'T FORGOT CREDITS ELSE MOTHERFUCKER, SISTER FUCKER, WITHOUT DICK GAY
